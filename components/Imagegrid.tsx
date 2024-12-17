225daa7107b0ac3af098f6648321d7c14b4d3a8a
kvishal04 <vishal.kumar@123789.org>
2024-12-17 15:12:42 +0530

    hjkl

--- components/Imagegrid.tsx
+++ components/Imagegrid.tsx
@@ -91,7 +91,7 @@ const ImageGrid = () => {
 
 
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-6 gap-6 p-6">
+    <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-6 p-6">
       {images.map((image, index) => (
         <div
           key={index}
