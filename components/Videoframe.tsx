9b7c69342f7207b403885867e546515daaff436c
kvishal04 <vishal.kumar@123789.org>
2025-01-13 02:54:48 +0530

    writting done

--- components/Videoframe.tsx
+++ components/Videoframe.tsx
@@ -14,12 +14,13 @@ const Videoframe = () => {
         {/* Video Section */}
         <div className="w-full h-auto rounded-lg shadow-2xl">
         <div
-          dangerouslySetInnerHTML={{
-            __html: `<video className="app__backgroundVideo" autoplay loop muted playsinline>
-      <source src='/assets/bday/eyes.mp4' type="video/mp4" />
-      Your browser does not support the video tag.
-</video>`,
-          }}
+        dangerouslySetInnerHTML={{
+          __html: `<video class="app__backgroundVideo" autoplay loop muted playsinline>
+            <source src='/assets/bday/eyes.mp4' type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>`,
+        }}
+        
         />
           {/* <video
             className="w-full h-auto rounded-lg shadow-2xl"
